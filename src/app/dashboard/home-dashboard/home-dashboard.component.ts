c8748f30c3d763654c0efa81742e041f7fd10b63
LucasViniciusLima <lucaslimavzt@gmail.com>
2022-03-16 19:45:44 -0300

    many graphycs

--- src/app/dashboard/home-dashboard/home-dashboard.component.ts
+++ src/app/dashboard/home-dashboard/home-dashboard.component.ts
@@ -7,29 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeDashboardComponent implements OnInit {
   saleData = [
-    { name: "02/02/2022", value: 105000 },//quantidade gasta naquele dia
-    { name: "13/02/2022", value: 55000 },
-    { name: "14/02/2022", value: 15000 },
-    { name: "11/03/2022", value: 150000 },
-    { name: "12/03/2022", value: 20000 },
-    { name: "14/03/2022", value: 105000 },
-    { name: "15/03/2022", value: 55000 },
-    { name: "16/03/2022", value: 15000 },
-    { name: "17/03/2022", value: 150000 },
-    { name: "18/03/2022", value: 20000 }
+    { name: "02/02", value: 10050 },//quantidade gasta naquele dia
+    { name: "13/02", value: 5500 },
+    { name: "14/02", value: 1000 },
+    { name: "11/03", value: 1900 },
+    { name: "12/03", value: 990 },
+    { name: "14/03", value: 10000 },
   ];
 
   quantsaleData = [
-    { name: "02/02/2022", value: 5 },//quantidade de itens comprados
-    { name: "13/02/2022", value: 10 },
-    { name: "14/02/2022", value: 15 },
-    { name: "11/03/2022", value: 8 },
-    { name: "12/03/2022", value: 2 },
-    { name: "14/03/2022", value: 1 },
-    { name: "15/03/2022", value: 5 },
-    { name: "16/03/2022", value: 10 },
-    { name: "17/03/2022", value: 15 },
-    { name: "18/03/2022", value: 20 }
+    { name: "02/02", value: 5 },//quantidade de itens comprados
+    { name: "13/02", value: 10 },
+    { name: "14/02", value: 15 },
+    { name: "11/03", value: 8 },
+    { name: "12/03", value: 2 },
+    { name: "14/03", value: 1 },
   ];
 
 
