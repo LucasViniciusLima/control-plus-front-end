bfc4ca280d2eeaee985a85f8ba026d8193054379
LucasViniciusLima <lucaslimavzt@gmail.com>
2022-03-16 15:17:15 -0300

    two dashboards

--- src/app/dashboard/home-dashboard/home-dashboard.component.ts
+++ src/app/dashboard/home-dashboard/home-dashboard.component.ts
@@ -7,12 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeDashboardComponent implements OnInit {
   saleData = [
-    { name: "Mobiles", value: 105000 },
-    { name: "Laptop", value: 55000 },
-    { name: "AC", value: 15000 },
-    { name: "Headset", value: 150000 },
-    { name: "Fridge", value: 20000 }
+    { name: "02/02/2022", value: 105000 },//quantidade gasta naquele dia
+    { name: "13/02/2022", value: 55000 },
+    { name: "14/02/2022", value: 15000 },
+    { name: "11/03/2022", value: 150000 },
+    { name: "12/03/2022", value: 20000 },
+    { name: "14/03/2022", value: 105000 },
+    { name: "15/03/2022", value: 55000 },
+    { name: "16/03/2022", value: 15000 },
+    { name: "17/03/2022", value: 150000 },
+    { name: "18/03/2022", value: 20000 }
   ];
+
+  quantsaleData = [
+    { name: "02/02/2022", value: 5 },//quantidade de itens comprados
+    { name: "13/02/2022", value: 10 },
+    { name: "14/02/2022", value: 15 },
+    { name: "11/03/2022", value: 8 },
+    { name: "12/03/2022", value: 2 },
+    { name: "14/03/2022", value: 1 },
+    { name: "15/03/2022", value: 5 },
+    { name: "16/03/2022", value: 10 },
+    { name: "17/03/2022", value: 15 },
+    { name: "18/03/2022", value: 20 }
+  ];
+
 
   constructor() { }
 
