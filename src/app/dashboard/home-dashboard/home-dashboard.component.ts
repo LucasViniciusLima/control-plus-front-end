d0c744bf915cc1592bbe3e9660c0c5420134d7c2
LucasViniciusLima <lucaslimavzt@gmail.com>
2022-06-19 15:17:46 -0300

    dashboard fix

--- src/app/dashboard/home-dashboard/home-dashboard.component.ts
+++ src/app/dashboard/home-dashboard/home-dashboard.component.ts
@@ -10,26 +10,12 @@ import { FluxoCaixaService } from 'src/app/shared/services/fluxo-caixa.service';
   styleUrls: ['./home-dashboard.component.css']
 })
 export class HomeDashboardComponent implements OnInit {
-  saleData = [
-    { name: "02/02", value: 1000050 },
-    { name: "13/02", value: 550000 },
-    { name: "14/02", value: 100000 },
-    { name: "11/03", value: 190000 },
-    { name: "12/03", value: 99000 },
-    { name: "14/03", value: 1000000 },
-  ];
-  //os gráficos carregam normalmente se os dados forem apresentados aos mesmos de forma unica sem necessidade de mais de uma requisição
-  quantsaleData = [
-    { name: "02/02", value: 5000000 },
-    { name: "13/02", value: 10000 },
-    { name: "14/02", value: 15000 },
-    { name: "11/03", value: 800000 },
-    { name: "12/03", value: 20000 },
-    { name: "14/03", value: 100000 },
-  ];
+  saleData = [];
+  costData = [];
+  quantsaleData = [];  
+  quantCostData = [];
 
   comparativoChart: Array<any> = [];
-
 
   constructor(private readonly fluxoCaixaService: FluxoCaixaService) { }
 
@@ -38,12 +24,77 @@ export class HomeDashboardComponent implements OnInit {
       .subscribe((custo) => {
         this.comparativoChart.push({ name: 'Custos', value: custo.total });
         this.comparativoChart = [...this.comparativoChart];
+
+        let custoList: any = [];
+        let quantCustoList: any = [];
+
+        custoList[0] = custo.listaCusto[0];
+        custoList[0].data = new Date(custoList[0].data).toLocaleString().substring(0, 10);
+
+        quantCustoList[0] = { name: custoList[0].data, value: 1 };
+
+
+        for (let i = 1; i < custo.listaCusto.length; i++) {
+
+          const itemReceita = custo.listaCusto[i];
+          itemReceita.data = new Date(itemReceita.data).toLocaleString().substring(0, 10);
+
+          let indexDataEncontrada = custoList.findIndex((item: any) => item.data == itemReceita.data);
+
+          if (indexDataEncontrada >= 0) {
+            custoList[indexDataEncontrada].preco += itemReceita.preco;
+            custoList[indexDataEncontrada].value++;
+          } else {
+            custoList.push(itemReceita);
+            quantCustoList.push({ name: itemReceita.data, value: 1 });
+          }
+
+        }
+
+
+        this.costData = custoList.map((item: any) => {
+          return { name: item.data, value: item.preco };
+        });
+        this.quantCostData = quantCustoList;
       });
 
     this.fluxoCaixaService.carregarFluxosReceita()
       .subscribe((receita) => {
         this.comparativoChart.push({ name: 'Receitas', value: receita.total });
         this.comparativoChart = [...this.comparativoChart];
+
+        let receitaList: any = [];
+        let quantReceitaList: any = [];
+
+        receitaList[0] = receita.listaReceita[0];
+        receitaList[0].data = new Date(receitaList[0].data).toLocaleString().substring(0, 10);
+
+        quantReceitaList[0] = { name: receitaList[0].data, value: 1 };
+
+
+        for (let i = 1; i < receita.listaReceita.length; i++) {
+
+          const itemReceita = receita.listaReceita[i];
+          itemReceita.data = new Date(itemReceita.data).toLocaleString().substring(0, 10);
+
+          let indexDataEncontrada = receitaList.findIndex((item: any) => item.data == itemReceita.data);
+
+          if (indexDataEncontrada >= 0) {
+            receitaList[indexDataEncontrada].preco += itemReceita.preco;
+            quantReceitaList[indexDataEncontrada].value++;
+          } else {
+            receitaList.push(itemReceita);
+            quantReceitaList.push({ name: itemReceita.data, value: 1 });
+          }
+
+        }
+
+
+        this.saleData = receitaList.map((item: any) => {
+          return { name: item.data, value: item.preco };
+        });
+        this.quantsaleData = quantReceitaList;
+
       });
   }
 
